d99df4af692e223c89c54659d08b40d4cb76bdf3
LCK <1977906158@qq.com>
2025-08-18 20:44:24 +0800

    INIT

---
+++ src/main/java/com/liuchangking/dreamtpa/DreamTPA.java
@@ -0,0 +1,17 @@
+package com.liuchangking.dreamtpa;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DreamTPA extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
